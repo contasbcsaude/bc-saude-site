6b9f995ac87d028a6f193d3a63077559cd2ba2ea
contasbcsaude <contasbcsaude@gmail.com>
2026-08-19 13:43:02 -0300

    fix: remove router dep, simplify App for GitHub Pages SPA

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Index from './pages/Index'
 import Diagnostico from './pages/Diagnostico'
-import NotFound from './pages/NotFound'
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/diagnostico" element={<Diagnostico />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  )
+  const path = window.location.pathname
+  if (path === '/diagnostico' || path === '/bc-saude-site/diagnostico') {
+    return <Diagnostico />
+  }
+  return <Index />
 }
 
 export default App
